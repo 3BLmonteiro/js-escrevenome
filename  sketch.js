0acffa806ae868c38a06decbfbea16971f32f261
3BLmonteiro <00001107790517sp@al.educacao.sp.gov.br>
2024-05-24 20:36:52 -0300

    ajuste sketch.js

---  sketch.js
+++  sketch.js
@@ -32,6 +32,7 @@ let colidiu = false;
 
 function setup() {
   createCanvas(600, 400);
+    trilha.loop();
 }
 
 function draw() {
@@ -132,12 +133,10 @@ function marcaPonto() {
         ponto.play();
     }
 }
+
+
 function preload(){
-
   trilha = loadSound("trilha.mp3");
-
   ponto = loadSound("ponto.mp3");
-
   raquetada = loadSound("raquetada.mp3");
-
 }
